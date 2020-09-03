cbae7b060b55d71a5e2afb424abe048484ad7eb2
Adam Faidy <faidyadam1@gmail.com>
2020-09-03 16:26:12 -0400

    first part of tictac js

---
+++ tic-tac-toe.js
@@ -0,0 +1,17 @@
+const currentPlayerSymbol = 'X';
+const squareValues = `[''], [''], [''], [''], [''], [''], [''], [''], ['']`;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    
+const ticTacHandler = document.getElementById('tic-tac-toe-board')
+ticTacHandler.addEventListener('click', (event))
+
+
+
+
+
+
+
+
+})
+
